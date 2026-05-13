11a72216e96a9f239f79b4749a87afd9d34947f2
maybe-adamant <maybe.adamant@gmail.com>
2026-05-13 10:41:23 -0400

    Declare checkbox storage defaults

--- src/data.lua
+++ src/data.lua
@@ -22,7 +22,7 @@ function data.buildStorage()
             table.insert(storage, {
                 type = "bool",
                 alias = option.alias,
-                default = option.default,
+                default = option.default == true,
             })
         else
             error(("Unsupported option type '%s' in %s"):format(tostring(option.type), PACK_ID .. ".QoL"))
