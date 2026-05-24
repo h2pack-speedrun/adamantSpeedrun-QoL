beb231bd3d058afece0057f2ac3f4aa33441f1e5
maybe-adamant <maybe.adamant@gmail.com>
2026-05-23 22:33:18 -0400

    minor code cleanup

--- src/ui.lua
+++ src/ui.lua
@@ -1,27 +1,34 @@
 local module = {}
 local data = nil
+local checkboxOptsByAlias = {}
 
-local function DrawOptions(draw, state)
+local function drawOptions(draw, state)
     for _, option in ipairs(data.options) do
         if option.type == "checkbox" then
-            draw.widgets.checkbox(state.get(option.alias), {
-                label = option.label,
-                tooltip = option.tooltip,
-            })
+            draw.widgets.checkbox(state.get(option.alias), checkboxOptsByAlias[option.alias])
         end
     end
 end
 
 function module.drawTab(draw, state)
-    DrawOptions(draw, state)
+    drawOptions(draw, state)
 end
 
 function module.drawQuickContent(draw, state)
-    DrawOptions(draw, state)
+    drawOptions(draw, state)
 end
 
 function module.bind(moduleData)
     data = moduleData
+    checkboxOptsByAlias = {}
+    for _, option in ipairs(data.options) do
+        if option.type == "checkbox" then
+            checkboxOptsByAlias[option.alias] = {
+                label = option.label,
+                tooltip = option.tooltip,
+            }
+        end
+    end
     return module
 end
 
